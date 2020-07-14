5080a6d54abe3da3830b7ca34a4c0c8cfbff06b9
DeepakGoswami5 <53327445+DeepakGoswami5@users.noreply.github.com>
2020-07-14 07:41:10 +0530

    'migrations'

--- social_site/social_site/urls.py
+++ social_site/social_site/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from .import views
 
 urlpatterns = [
+    path('accounts/',include('accounts.urls'),name='accounts'),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('',views.HomePage.as_view(),name='home'),
     path('admin/', admin.site.urls),
+    path('test',views.TestPage.as_view(),name='test'),
+    path('thanks',views.ThanksPage.as_view(),name='thanks'),
 ]
